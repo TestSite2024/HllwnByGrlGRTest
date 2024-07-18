49ce05af29ed4de063027a553482bab529479114
TestSite2024 <aykut.ana.karaca@gmail.com>
2024-07-18 21:06:46 +0300

    updated

--- main.js
+++ main.js
@@ -4,7 +4,12 @@
  * depends on jQuery>=1.7
  */
 var pumpkin;
-
+var rnd;
+// locations of correct gender circles
+var loc = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
+// location of other gender which will give scratch further warning
+var oloc = [[4,5,9],[1,2,7],[1,3,4],[3,5,8],[1,4,9],[1,2,7],[3,4,7],[1,2,6]];
+var pct =new Array(9);
 (function() {
     /**
      * Returns true if this browser supports canvas
@@ -43,28 +48,37 @@ var pumpkin;
      * Handle scratch event on a scratcher
      */
     function checkpct() {
+        var pct1 = pct[loc[rnd-1][0]-1];
+        var pct2 = pct[loc[rnd-1][1]-1];
+        var pct3 = pct[loc[rnd-1][2]-1];
+
+        var pct4= pct[oloc[rnd-1][0]-1];
+        var pct5= pct[oloc[rnd-1][1]-1];
+        var pct6= pct[oloc[rnd-1][2]-1];
+
         if (!triggered) {
-            if (pct3>0 && pct4>0 && pct5>0)  {
-                if (pct3<15 || pct4<15 || pct5<15)  {
+            if (pct1>0 && pct2>0 && pct3>0)  {
+                if (pct1<15 || pct2<15 || pct<15)  {
                 //document.getElementById("scratcher3Pct").innerHTML="Scratch MORE!";
                 if (!CrispyToast.clearall()){
                     CrispyToast.success('Scratch MORE!',{ position: 'top-center' },{timeout: 3000});
                     }
                 } 
             }
-            if ((pct1>15 && pct2>15 && pct6>15)&&(pct3<15||pct4<15||pct5<15)) {
+            if ((pct4>15 && pct5>15 && pct6>15)&&(pct1<15||pct2<15||pct3<15)) {
                 if (!CrispyToast.clearall()&&!triggered){
                     CrispyToast.error('Scratch other circles. You havent find the gender yet!',{ position: 'top-center' },{timeout: 6000});
                     }
             } 
-            if (pct3>15&& pct4>15 && pct5>15) {
+
+            if (pct1>15&& pct2>15 && pct3>15) {
                 $('#boy').text(gendertext);
                 $('#boy').css('color',colortxt);
                 $('#or').hide();
                 $('#girl').hide();
                 document.getElementsByTagName("body")[0].style.backgroundColor = color;
                 document.getElementsByTagName("body")[0].style.backgroundImage = 'none';
-                document.getElementById("H3").insertAdjacentHTML('afterend', "<h4 id='testtext' style='white-space:normal'> Depending on the product you buy, here it will say either <br> 'It is a Girl!' or 'It is a Boy! with pink or blue background.</h4>");
+                //document.getElementById("H3").insertAdjacentHTML('afterend', "<h4 id='testtext' style='white-space:normal'> Depending on the product you buy, here it will say either <br> 'It is a Girl!' or 'It is a Boy! with pink or blue background.</h4>");
 
                 $('#H3').hide();
                 $('#H4').hide();
@@ -75,11 +89,11 @@ var pumpkin;
         }
     };
     function scratcher1Changed(ev) {
-        pct1 = (this.fullAmount(40) * 100)|0;
+        pct[0] = (this.fullAmount(40) * 100)|0;
         checkpct();
     };
     function scratcher2Changed(ev) {
-        pct2 = (this.fullAmount(40) * 100)|0;
+        pct[1] = (this.fullAmount(40) * 100)|0;
         checkpct();
     };
     function scratcher3Changed(ev) {
@@ -90,24 +104,39 @@ var pumpkin;
         // Only test every 32nd pixel. 32x faster, but might lead to
         // inaccuracy:
 
-        pct3 = (this.fullAmount(40) * 100)|0;
+        pct[2] = (this.fullAmount(40) * 100)|0;
         checkpct();
         
     };
     function scratcher4Changed(ev) {
-        pct4 = (this.fullAmount(40) * 100)|0;
+        pct[3] = (this.fullAmount(40) * 100)|0;
         checkpct();
     };
     function scratcher5Changed(ev) {
-        pct5 = (this.fullAmount(40) * 100)|0;
+        pct[4] = (this.fullAmount(40) * 100)|0;
        checkpct();
     };
     function scratcher6Changed(ev) {
-        pct6 = (this.fullAmount(40) * 100)|0;
+        pct[5]= (this.fullAmount(40) * 100)|0;
+        checkpct();
+    };
+    function scratcher7Changed(ev) {
+        pct[6] = (this.fullAmount(40) * 100)|0;
+        checkpct();
+    };
+    function scratcher8Changed(ev) {
+        pct[7] = (this.fullAmount(40) * 100)|0;
+        checkpct();
+    };
+    function scratcher9Changed(ev) {
+        pct[8] = (this.fullAmount(40) * 100)|0;
         checkpct();
     };
     function randomInRange(min, max) {
         return Math.random() * (max - min) + min;
+    };
+    function randomInRangeint(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
     };
     function confetti_effect() {
         if(triggered==true) {
@@ -119,48 +148,49 @@ var pumpkin;
         }
         triggered=true;
         // do this for 10 seconds
-        var duration = 8 * 1000;
-        var end = Date.now() + duration;
-        var defaults = {
-            startVelocity: 20, 
-            spread: 180, 
-            ticks: 40, 
-            zIndex: 0, 
-            //scalar:2,
-            colors: ['#ffffff'],
-            //colors: ['#ff9a00', '#ff7400'],
-            shapes: [pumpkin]
-         };
-         var particleCount = 3 ;
+        var end = Date.now() + (5 * 1000);
+        var colors = ['#ff912a', '#ffffff'];
+
         (function frame() {
-        // launch a few confetti from the left edge
 
-        // and launch a few from the right edge
-        confetti({ ...defaults,  particleCount, scalar: randomInRange(1, 2.5), origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } });
-        confetti({ ...defaults,  particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } });
+            confetti({
+                particleCount: 2,
+                angle: 60,
+                spread: 55,
+                scalar: 2,
+                shapes: [pumpkin],
+                origin: { x: 0, y:1 },
+                startVelocity: 150,
+                colors: colors
+            });
+            confetti({
+                particleCount: 2,
+                angle: 120,
+                spread: 55,
+                startVelocity: 150,
+                origin: { x: 1, y:1},
+                colors: colors
+            });
 
-        // keep going until we are out of time
-        if (Date.now() < end) {
-            requestAnimationFrame(frame);
-            return;
-        }
-        $("#resetbutton").show();
-        
+
+            // keep going until we are out of time
+            if (Date.now() < end) {
+                requestAnimationFrame(frame);
+
+                return;
+            }
+            $("#resetbutton").show();
+
         }());
-             
-    };
+              
+     };
     
     /**
      * Reset all scratchers
      */
     function onResetClicked(scratchers) {
         var i;
-        pct1=0;
-        pct2=0;
-        pct3=0;
-        pct4=0;
-        pct5=0;
-        pct6=0;
+        pct = [];
         $("#scratcher3Pct").hide();
         $("#resetbutton").hide();
         for (i = 0; i < scratchers.length; i++) {
@@ -213,6 +243,7 @@ var pumpkin;
     function initPage() {
         var scratcherLoadedCount = 0;
         var scratchers = [];
+        var pct = [];
         var i, i1;
         
         // if (window.confirm('This scratch off contains sound when the gender is revealed. Do you want to continue with sound? (Ok:with sound, Cancel:without sound')) {
@@ -291,7 +322,10 @@ var pumpkin;
     
         // create new scratchers
         var scratchers = new Array(9);
-    
+        rnd = 2;
+        if (rnd>8) {
+            rnd=8;   
+        }
         for (i = 0; i < scratchers.length; i++) {
             i1 = i + 1;
             scratchers[i] = new Scratcher('scratcher' + i1);
@@ -299,7 +333,7 @@ var pumpkin;
             // set up this listener before calling setImages():
             scratchers[i].addEventListener('imagesloaded', onScratcherLoaded);
     
-            scratchers[i].setImages('images/s' + i1 + 'bg.jpg',
+            scratchers[i].setImages('images/' + rnd + '/s' + i1 + 'bg.jpg',
                 'images/foreground.jpg');
         
         }
@@ -310,12 +344,17 @@ var pumpkin;
         //scratchers[3].addEventListener('reset', scratchersChanged);
         scratchers[0].addEventListener('scratchesended', scratcher1Changed);
         scratchers[1].addEventListener('scratchesended', scratcher2Changed);
-        scratchers[3].addEventListener('scratchesended', scratcher3Changed);
+        scratchers[2].addEventListener('scratchesended', scratcher3Changed);
+
+        scratchers[3].addEventListener('scratchesended', scratcher4Changed);
         //scratchers[4].addEventListener('reset', scratchersChanged);
-        scratchers[4].addEventListener('scratchesended', scratcher4Changed);
+        scratchers[4].addEventListener('scratchesended', scratcher5Changed);
         //scratchers[5].addEventListener('reset', scratchersChanged);
-        scratchers[5].addEventListener('scratchesended', scratcher5Changed);
-        scratchers[6].addEventListener('scratchesended', scratcher6Changed);
+        scratchers[5].addEventListener('scratchesended', scratcher6Changed);
+        scratchers[6].addEventListener('scratchesended', scratcher7Changed);
+        scratchers[7].addEventListener('scratchesended', scratcher8Changed);
+        scratchers[8].addEventListener('scratchesended', scratcher9Changed);
+
         var canvas = document.getElementById('scratcher1');
         canvas.onmousemove = null;
         // Or if you didn't want to do it every scratch (to save CPU), you
