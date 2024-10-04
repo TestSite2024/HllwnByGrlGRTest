b8bf53c85273b4e3a8063a146c0c2a942c71a46d
TestSite2024 <aykut.ana.karaca@gmail.com>
2024-10-04 12:32:00 +0300

    Corrected bold text in modal

--- main.js
+++ main.js
@@ -259,6 +259,7 @@ var pct =new Array(9);
         else {
             $("#baby").text('the baby!');
             surname="the";
+            document.getElementById('surname').style.fontWeight="normal";
         }
 /*        $(document).ready(function(){
             // function scope wavesurfer
@@ -267,7 +268,9 @@ var pct =new Array(9);
                 matrix: [0.35277776,0,0,-0.35277778,35.416827,22.91533]
               });
         }); */
-        document.getElementById('intro').innerHTML= "This is a gender reveal scratch off for <strong>" + surname + "</strong> family. It contains sound when the gender is revealed. Do you want to continue with sound?";
+        //document.getElementById('intro').innerHTML= "This is a gender reveal scratch off for <strong>" + surname + "</strong> family. It contains sound when the gender is revealed. Do you want to continue with sound?";
+        document.getElementById('surname').innerHTML= surname;
+
         document.getElementById('id01').style.display='block';
         $('.nosoundbtn').on("click", function (e) {
             document.getElementById('id01').style.display='none';
